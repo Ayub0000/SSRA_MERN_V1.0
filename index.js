84f600e205520e07f729da6fb63d63236febe62a
Ayub <ayubkiry436@gmail.com>
2023-08-30 22:16:17 +0300

    server fixed

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const path = require('path ')
+const path = require('path')
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + "public ")))
+app.use(express.static(path.join(__dirname + "/public")))
 
-const PORT = process.env.PORT ||  5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log('app is listning on port 3000'); 
+    console.log(`server is running on port ${PORT}`)
 })
